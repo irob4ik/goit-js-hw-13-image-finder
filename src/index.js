a3b7130631bd8ef946e62fe013956d43c95ed007
Roberts Kurmis <theroberto@inbox.lv>
2021-05-29 23:50:33 +0300

    added search API image, finished marking gallery, added styles

--- src/index.js
+++ src/index.js
@@ -1,1 +1,70 @@
 import './sass/main.scss';
+import 'regenerator-runtime/runtime';
+
+import { error } from '@pnotify/core';
+import "../node_modules/@pnotify/core/dist/PNotify.css";
+import "../node_modules/@pnotify/core/dist/BrightTheme.css";
+
+import _ from 'lodash';
+
+import galleryCards from './templates/gallery_cards.hbs';
+import fetchImage from './js/apiService';
+
+const refs = {
+    searchForm: document.querySelector('#js-search-form'),
+    galleryContainer: document.querySelector('#js-gallery'),
+    loadMoreBtn: document.querySelector('[data-action="load-more"]'),
+    sQuery: '',
+    pageToLoad: 1,    
+}
+
+refs.searchForm.addEventListener('input', _.debounce(onSearch, 500));
+refs.loadMoreBtn.addEventListener('click', onClick);
+
+function onSearch(e) {
+    clearMarkup();
+    e.preventDefault();
+    refs.sQuery = e.target.value;
+    refs.pageToLoad = 1;
+
+    if (refs.sQuery.trim() === '') {
+        return error({
+            text: "Please type something..",
+            delay: 1500
+        });
+    }
+    
+    fetchRequest();
+
+    refs.loadMoreBtn.classList.remove("is-hidden");    
+}
+
+async function fetchRequest() {
+    try {
+        const gallery = await fetchImage(refs.sQuery, refs.pageToLoad);
+        createMarkup(gallery.hits);
+    } catch (e) {
+        console.log(e);
+
+        return error({
+        text: "Country not found",
+        delay: 1500
+        });
+    }
+}
+
+function createMarkup(galleryList) {
+    const galleryMarkup = galleryList.map(galleryCards).join('');
+    refs.galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+}
+
+function onClick() {
+    refs.pageToLoad += 1;
+    fetchRequest();
+}
+
+function clearMarkup() {
+    refs.galleryContainer.innerHTML = "";
+    refs.loadMoreBtn.classList.add("is-hidden");
+}
+
